c4c00356b73457356aa11c517f1cd2ed346e9c12
lyqmine <964535483@qq.com>
2025-01-06 00:29:10 +0800

    fix code

--- rustlings/exercises/04_primitive_types/primitive_types6.rs
+++ rustlings/exercises/04_primitive_types/primitive_types6.rs
@@ -1,5 +1,6 @@
 fn main() {
     // You can optionally experiment here.
+    indexing_tuple();
 }
 
 #[cfg(test)]
@@ -11,7 +12,7 @@ mod tests {
         // TODO: Use a tuple index to access the second element of `numbers`
         // and assign it to a variable called `second`.
         // let second = ???;
-
+        let second=number[1];
         assert_eq!(second, 2, "This is not the 2nd number in the tuple!");
     }
 }
