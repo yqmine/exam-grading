a729c7f4347939e3b34a643408622f3249687946
lyqmine <964535483@qq.com>
2025-01-06 22:56:25 +0800

    fix code

--- rustlings/exercises/11_hashmaps/hashmaps1.rs
+++ rustlings/exercises/11_hashmaps/hashmaps1.rs
@@ -9,13 +9,20 @@ use std::collections::HashMap;
 fn fruit_basket() -> HashMap<String, u32> {
     // TODO: Declare the hash map.
     // let mut basket =
-
+    let mut basket=HashMap::new();
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket.
 
-    basket
+    basket.insert(String::from("a"), 2);
+    basket.insert(String::from("b"), 2);
+    basket.insert(String::from("c"), 2);
+    basket.insert(String::from("d"), 2);
+    basket.insert(String::from("e"), 2);
+    basket.insert(String::from("f"), 2);
+    basket.insert(String::from("g"), 2);
+    
 }
 
 fn main() {
--- rustlings/exercises/11_hashmaps/hashmaps3.rs
+++ rustlings/exercises/11_hashmaps/hashmaps3.rs
@@ -31,6 +31,23 @@ fn build_scores_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
+        if !scores.contains_key(team_1_name){
+            scores.insert(team_1_name,TeamScores{goals_scored:team_1_score,goals_conceded:team_2_score});
+        }else{
+            if let Some(x) = scores.get_mut(team_1_name) {
+                x.goals_scored +=team_1_score;
+                x.goals_conceded +=team_2_score;
+            }
+        }
+
+        if !scores.contains_key(team_2_name){
+            scores.insert(team_2_name,TeamScores{goals_scored:team_2_score,goals_conceded:team_1_score});
+        }else{
+            if let Some(x) = scores.get_mut(team_2_name) {
+                x.goals_scored +=team_2_score;
+                x.goals_conceded +=team_1_score;
+            }
+        }
     }
 
     scores
