4ffd457d9a63454259f20d1facb1c376c0367f65
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 <964535483@qq.com>
2025-01-09 19:36:15 +0800

    fix code

--- rustlings/exercises/21_macros/macros3.rs
+++ rustlings/exercises/21_macros/macros3.rs
@@ -1,7 +1,8 @@
 // TODO: Fix the compiler error without taking the macro definition out of this
 // module.
+#[macro_use]
 mod macros {
-    macro_rules! my_macro {
+      macro_rules! my_macro {
         () => {
             println!("Check out my macro!");
         };
