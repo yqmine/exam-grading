7eb03feaf6f4b42946eabc79c3d3d2dd25a7a470
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 <964535483@qq.com>
2025-01-09 19:47:30 +0800

    fix code

--- rustlings/exercises/22_clippy/clippy2.rs
+++ rustlings/exercises/22_clippy/clippy2.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let mut res = 42;
+    let mut res:i32 = 42;
     let option = Some(12);
     // TODO: Fix the Clippy lint.
-    for x in option {
+    if let Some(x) = option {
         res += x;
     }
 
