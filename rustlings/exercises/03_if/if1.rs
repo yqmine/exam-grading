fd0f1b3134f055e3cca7e1b262d5937fbe23582b
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 <964535483@qq.com>
2025-01-10 19:41:50 +0800

    fix code

--- rustlings/exercises/03_if/if1.rs
+++ rustlings/exercises/03_if/if1.rs
@@ -4,7 +4,7 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
-    if(a > b) {
+    if a > b {
         return a;
     }
     else{
