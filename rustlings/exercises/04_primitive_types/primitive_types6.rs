d2fde572465ee49e999a26ff5ec743504852b651
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 <964535483@qq.com>
2025-01-10 19:50:14 +0800

    fix code

--- rustlings/exercises/04_primitive_types/primitive_types6.rs
+++ rustlings/exercises/04_primitive_types/primitive_types6.rs
@@ -12,7 +12,7 @@ mod tests {
         // TODO: Use a tuple index to access the second element of `numbers`
         // and assign it to a variable called `second`.
         // let second = ???;
-        let second=number[1];
+        let second=number.1;
         assert_eq!(second, 2, "This is not the 2nd number in the tuple!");
     }
 }
