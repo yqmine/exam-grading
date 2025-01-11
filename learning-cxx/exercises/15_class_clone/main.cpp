526ce323ff26777d4adfe051f1c61630733b6bdd
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 <964535483@qq.com>
2025-01-11 11:38:33 +0800

    fix code

--- learning-cxx/exercises/15_class_clone/main.cpp
+++ learning-cxx/exercises/15_class_clone/main.cpp
@@ -59,7 +59,7 @@ public:
 int main(int argc, char **argv) {
     DynFibonacci fib(12);
     ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
-    DynFibonacci const fib_ = fib;
+    DynFibonacci  fib_ = fib;
     ASSERT(fib_.get(10) == fib.get(10), "Object cloned");
     return 0;
 }
