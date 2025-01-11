4e7d64645bb39cbd9feccb88bdc3a232b227ac41
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 <964535483@qq.com>
2025-01-11 09:54:59 +0800

    fix code

--- rustlings/exercises/05_vecs/vecs1.rs
+++ rustlings/exercises/05_vecs/vecs1.rs
@@ -4,13 +4,12 @@ fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
     // Use the vector macro.
     // let v = ???;
-    let v=vec![1,2,3];
+    let v=vec![10, 20, 30, 40];
     (a, v)
 }
 
 fn main() {
     // You can optionally experiment here.
-    
 }
 
 #[cfg(test)]
