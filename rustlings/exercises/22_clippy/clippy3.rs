339db69ab7cc1bb577eb37f5293d3161f54bd9e2
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 <964535483@qq.com>
2025-01-11 15:42:16 +0800

    fix code

--- rustlings/exercises/22_clippy/clippy3.rs
+++ rustlings/exercises/22_clippy/clippy3.rs
@@ -1,6 +1,6 @@
 // Here are some more easy Clippy fixes so you can see its utility 📎
 // TODO: Fix all the Clippy lints.
-
+use std::mem;
 #[rustfmt::skip]
 #[allow(unused_variables, unused_assignments)]
 fn main() {
@@ -24,7 +24,7 @@ fn main() {
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    use std::mem;
+
     mem::swap(&mut value_a, &mut value_b);
     println!("value a: {value_a}; value b: {value_b}");
 }
